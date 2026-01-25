6eb0d3d0d472862d81b93bd4da4a44c68c6acb15
ramananr49 <venketramanan@live.in>
2026-01-25 23:14:00 +0530

    Created the test script to handle the various functionality in text field

---
+++ tests/InputFiels.spec.js
@@ -0,0 +1,27 @@
+import {test, expect} from '@playwright/test'
+
+test("Handling Input Field related scenario in PW_JS", async ({page}) => {
+    await page.goto("https://letcode.in/");
+    await page.locator('a[id="testing"][href="/test"]').click();
+    await expect(page.locator('.card-header p', {hasText: 'Input'})).toBeVisible();
+    const input_content = await page.locator('//*[normalize-space()="Input"]/parent::header/following-sibling::div//p').textContent();
+    expect(input_content).toContain("Interact with different types of input fields");
+    await page.locator('a[href="/edit"]').click();
+    await expect(page.locator('h1[class*="title"]')).toHaveText("Input");
+    await page.locator("#fullName").fill("Ramanan Ramasamy");
+    await page.locator("#join").click();
+    await page.locator("#join").type(" Tester");
+    await expect(page.locator("#join")).toHaveValue("I am good Tester");
+    await page.keyboard.press('Tab');
+    const exp_getText = await page.locator("#getMe").innerText();
+    expect(exp_getText).toEqual("ortonikc");
+    const olf_text = await page.locator("#clearMe").toHaveText();
+    await page.locator("#clearMe").clear();
+    await page.locator("#clearMe").fill("Ramanan Ramasamy");
+    expect(olf_text).toEqual("Koushik Chatterjee");
+    await expect(page.locator("#clearMe")).toHaveValue("Ramanan Ramasamy");
+    await expect(page.locator("#noEdit")).toBeDisabled();
+    await expect(page.locator("#dontwrite")).toHaveAttribute("readonly");
+    await page.close();
+
+})
