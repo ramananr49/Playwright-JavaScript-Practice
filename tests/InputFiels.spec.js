dcb7380f79e062d367642a6fe793ce33c484b3fb
ramananr49 <venketramanan@live.in>
2026-01-25 23:22:10 +0530

    Created the test script to handle the various functionality in text field

--- tests/InputFiels.spec.js
+++ tests/InputFiels.spec.js
@@ -7,15 +7,15 @@ test("Handling Input Field related scenario in PW_JS", async ({page}) => {
     const input_content = await page.locator('//*[normalize-space()="Input"]/parent::header/following-sibling::div//p').textContent();
     expect(input_content).toContain("Interact with different types of input fields");
     await page.locator('a[href="/edit"]').click();
-    await expect(page.locator('h1[class*="title"]')).toHaveText("Input");
+    await expect(page.locator('h1[class*="title"]')).toHaveText("Input", {timeout: 10000});
     await page.locator("#fullName").fill("Ramanan Ramasamy");
     await page.locator("#join").click();
     await page.locator("#join").type(" Tester");
     await expect(page.locator("#join")).toHaveValue("I am good Tester");
     await page.keyboard.press('Tab');
-    const exp_getText = await page.locator("#getMe").innerText();
+    const exp_getText = await page.locator("#getMe").inputValue();
     expect(exp_getText).toEqual("ortonikc");
-    const olf_text = await page.locator("#clearMe").toHaveText();
+    const olf_text = await page.locator("#clearMe").inputValue();
     await page.locator("#clearMe").clear();
     await page.locator("#clearMe").fill("Ramanan Ramasamy");
     expect(olf_text).toEqual("Koushik Chatterjee");
